ae7b3746ffaeffe5e281e360e80cf185b95b279f
jorgeloaiza48 <jelm48@misena.edu.co>
2024-11-25 10:06:29 -0500

    Arreglando las tarjetas individuales

--- src/components/RickyIndividual.js
+++ src/components/RickyIndividual.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 export default function RickyIndividual() {
@@ -11,11 +12,17 @@ export default function RickyIndividual() {
     .then((response) => setIndividual(response))
 
   return (
-    <div>
-      <img src={individual.image} alt={individual.name} />
-      <p>Nombre: {individual.name}</p>
-      <p>Status: {individual.status}</p>
-      <p>Especie: {individual.species}</p>
+
+    <div className='padreindividual'>
+      <div className='hijoindividual'>
+        <img className="imagenhijo" src={individual.image} alt={individual.name} />
+        <h1>Id: {individual.id}</h1>
+        <p>Nombre: {individual.name}</p>
+        <p>Status: {individual.status}</p>
+        <p>Especie: {individual.species}</p>
+        <p>Gender: {individual.gender}</p>
+        <center><Link to={"/"}><button>Regresar</button></Link></center>
+      </div>
     </div>
   )
 }
